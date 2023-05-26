18af6b5e281e442c6dc58ff12154b3f3c93af85d
hung nguyen <hungnguyenisme84@gmail.com>
2023-05-27 00:18:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baitapbuoi4/constraintLayout_1.java
@@ -0,0 +1,14 @@
+package com.example.baitapbuoi4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class constraintLayout_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
---
+++ app/src/main/java/com/example/baitapbuoi4/relative_layout_2.java
@@ -0,0 +1,14 @@
+package com.example.baitapbuoi4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class relative_layout_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout2);
+    }
+}
